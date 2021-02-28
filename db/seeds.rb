0495c41be3366bde4347b7603270c73ca6b9dd52
Yitzy Bratt <yitzybratt32@gmail.com>
2021-02-28 13:32:49 -0500

    adds 102 songs using seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  song = Song.create(title: FFaker::Music.song, artist: FFaker::Music.artist, album: FFaker::Music.album, genre: FFaker::Music.genre)
+  song.save
+end
